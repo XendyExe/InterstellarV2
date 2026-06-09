a98d6f71445a9609db7a52dd862ee27b3d717050
Xendy <xendyexe@gmail.com>
2026-06-08 20:57:51 -0700

    v2.6.1

--- Interstellar/Graphical/NavZone.ts
+++ Interstellar/Graphical/NavZone.ts
@@ -1,5 +1,13 @@
 import Interstellar from "../Interstellar";
-import Zone, { SubZone } from "./Zone";
+import Zone, {SubZone} from "./Zone";
+
+const NAV_ZONE_INDEXES: Record<number, number> = {
+    10: 0, // Hummingbird
+    20: 1, // Finch
+    30: 2, // Sparrow
+    40: 3, // Raven
+    50: 4, // Falcon
+}
 
 export default class NavZone extends Zone {
     navDefault: number = 1;
@@ -7,8 +15,6 @@ export default class NavZone extends Zone {
         super(subzones, currentIndex);
     }
     update(): void {
-        let target = Interstellar.patcher.navDestination - 1;
-        if (target < 0 || target > 4) target = this.navDefault;
-        this.transitionTarget = target;
+        this.transitionTarget = NAV_ZONE_INDEXES[Interstellar.patcher.navDestination] ?? this.navDefault;
     }
 }
